a6e230a870007e31d87a63d2aff45b3d4730009f
shiv-N <saurabh.navdkar@gmail.com>
2020-01-27 20:09:03 +0530

    add note image

---
+++ FundooSolution/CommonLayerModel/NotesModels/Request/UploadImage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayerModel.NotesModels.Request
+{
+    public class UploadImage
+    {
+        public IFormFile file { get; set; }
+    }
+}
